922fda92b3ec031479a112fecf403f77bfb01642
happyday326 <xcma1993@outlook.com>
2020-07-07 16:30:54 +0800

    Initial commit

---
+++ src/main/java/com/imooc/luckymoney/HelloController.java
@@ -0,0 +1,22 @@
+package com.imooc.luckymoney;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class HelloController {
+
+    @Value("${minMoney}")
+    private BigDecimal minMoney;
+
+    @Value("${description}")
+    private String description;
+
+    @GetMapping("/hello")
+    public String say() {
+        return "minMoney:" + minMoney + ", description: " + description;
+    }
+}
